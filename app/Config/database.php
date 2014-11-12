39c3f4677d40db9ea36220161ce7010c4dfbbe06
Guilherme <guitimoteo@yahoo.com.br>
2014-11-12 02:18:23 -0200

    adição do banco de dados

---
+++ app/Config/database.php
@@ -0,0 +1,22 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $n3script = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'n3script',
+	);
+        public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'n3script',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}
